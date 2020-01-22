6e5f3df892fe96b47cb1af0d9c4d1922b522c4dd
sbertrand14 <sbertrand14@laposte.net>
2020-01-22 18:07:56 +0100

    temporary commmit for 22/1

---
+++ app/models.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+
+class Tweet():
+    def __init__(self,text):
+        self.text = text
+        self.created_at = datetime.now()
---
+++ app/repositories.py
@@ -0,0 +1,10 @@
+
+class TweetRepository():
+    def __init__(self):
+        self.tweets = {}
+
+    def add(self,tweet):
+        pass
+
+    def get(self,index):
+        pass
